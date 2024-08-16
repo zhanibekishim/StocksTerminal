36699197de8dffb7ef8b59f7859e3a89cd5781e6
zhanibekishimss <230103150@sdu.edu.kz>
2024-08-16 05:15:46 +0500

    Path Effect using dash

--- app/src/main/java/com/example/terminal/presentation/Terminal.kt
+++ app/src/main/java/com/example/terminal/presentation/Terminal.kt
@@ -1,5 +1,7 @@
 package com.example.terminal.presentation
 
+
+
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.TransformableState
@@ -15,7 +17,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.unit.dp
 import com.example.terminal.data.model.BarDto
 import kotlin.math.roundToInt
 
@@ -84,6 +89,56 @@ fun Terminal(bars: List<BarDto>) {
                     strokeWidth = barWidth / 2
                 )
             }
+            bars.firstOrNull()?.let {
+                drawPrices(
+                    min = min,
+                    pxPerPoint = pxPerPoint,
+                    lastPrice = it.close
+                )
+            }
         }
     }
+}
+
+private fun DrawScope.drawPrices(
+    min: Float,
+    pxPerPoint: Float,
+    lastPrice: Float
+) {
+    // max
+    drawDashedLine(
+        start = Offset(0f, 0f),
+        end = Offset(size.width, 0f),
+    )
+
+    // last price
+    drawDashedLine(
+        start = Offset(0f, size.height - ((lastPrice - min) * pxPerPoint)),
+        end = Offset(size.width, size.height - ((lastPrice - min) * pxPerPoint)),
+    )
+
+    // min
+    drawDashedLine(
+        start = Offset(0f, size.height),
+        end = Offset(size.width, size.height),
+    )
+}
+
+private fun DrawScope.drawDashedLine(
+    color: Color = Color.White,
+    start: Offset,
+    end: Offset,
+    strokeWidth: Float = 1f
+) {
+    drawLine(
+        color = color,
+        start = start,
+        end = end,
+        strokeWidth = strokeWidth,
+        pathEffect = PathEffect.dashPathEffect(
+            intervals = floatArrayOf(
+                4.dp.toPx(), 4.dp.toPx()
+            )
+        )
+    )
 }
